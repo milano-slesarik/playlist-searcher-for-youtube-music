47ae1c7084d882aba1743c0343b58bf4c980187b
Milan Slesarik <milslesarik+github@gmail.com>
2025-09-26 00:50:49 +0200

    add search icon to sidebar input

--- src/templates.js
+++ src/templates.js
@@ -1,6 +1,21 @@
+const magGlassSvg = `
+<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
+  <path 
+    d="M16.296 16.996a8 8 0 11.707-.708l3.909 3.91-.707.707-3.909-3.909zM18 11a7 7 0 00-14 0 7 7 0 1014 0z"
+    fill="currentColor"
+    stroke="currentColor"
+    stroke-width="0.5"
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+  />
+</svg>`;
+
 const templates = {
     'sidebar-search-wrapper': `
         <div class="sidebar-search-wrapper">
+            <span class="search-icon" aria-hidden="true">
+                ${magGlassSvg}
+            </span>
             <input type="text" id="sidebar-playlist-search-input" placeholder="Search playlists" autocomplete="off">
         </div>
     `,
